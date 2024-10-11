1e07f03b10c54f1a5f039e8bbd0ff5b089fc8424
Matias Ludueña <matiasluduena23@gmail.com>
2024-10-11 13:06:37 -0300

    configure medico id in event

--- app/profesional/page.tsx
+++ app/profesional/page.tsx
@@ -22,7 +22,7 @@ export default async function page() {
     where: {
       OR: [
         {
-          medicosId: "jkdsfisdf9",
+          medicosId: "kssjjk",
         },
         {
           estado: "VACANTE",
