bdce7b87be5dcbc82bad1afa64848e8d76ef00b4
Matias Ludueña <matiasluduena23@gmail.com>
2024-10-11 12:57:46 -0300

    change medico profile

--- app/profesional/page.tsx
+++ app/profesional/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 export default async function page() {
   const medico = await prisma.medicos.findUnique({
     where: {
-      id: "kssjjk",
+      id: "kjjssk",
     },
   });
 
